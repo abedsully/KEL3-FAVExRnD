9e4609c28cfe322a95262cb7182ee3517452396a
abedsully <abedw2909@gmail.com>
2023-03-13 09:51:46 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
