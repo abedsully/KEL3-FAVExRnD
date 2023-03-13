2a266194e47fd0e9f1bdd34a9f51a0ad86965fac
abedsully <abedw2909@gmail.com>
2023-03-14 00:05:15 +0700

    added logo and edit title

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/report.css') }}">
+    <link rel="icon" href="/image/login/icon.svg" sizes="any" type="image/svg+xml">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
       </style>
-    <title>Report</title>
+    <title>Report • Favebook</title>
 </head>
 <body>
     <nav>
