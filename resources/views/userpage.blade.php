f042bd234faa1002215ffccdd35ba8473ae66882
Xelestiall <axelchris2004@gmail.com>
2023-04-07 22:01:43 +0700

    update userpage

--- resources/views/userpage.blade.php
+++ resources/views/userpage.blade.php
@@ -13,41 +13,53 @@
         </style>
     </head>
     <body>
-        <div class="background">
-        <nav class="navbar">
-            <div class="logo">
-                <img src="image/login/logo.png" alt="Company Logo">
-            </div>
-            <div class="profile-picture">
-                <img src="assets/pp.png" alt="Profile Picture">
-            </div>
-        </nav>
-        <section class="user-profile">
-            <div class="pp">
-                <img src="assets/pp2.png" alt="User Profile Picture">
-            </div>
-            <div class="user-info">
-                <h2>Mr. Chadest</h2>
-                <p>10 post</p>
-                <form action="editprofile##" class="style-button">
-                    <button type="submit"><i class="fa-solid fa-pen"></i>Edit profile</button>
-                </form>
-            </div>
-        </section>
-        <section class="my-post">
-            <div class="my-postingan">
-                <h3>My Posts</h3>
-                <hr>
-            </div>
-            <div class="post-wrapper">
-                <div class="wrapper1">
-
+            <nav class="navbar">
+                <div class="logo">
+                    <a href="/"><img src="image/login/logo.png" alt="Company Logo"></a>
                 </div>
-                <div class="wrapper2">
-
-                </div class="wrapper3">
-            </div>
-        </section>
+                <div class="profile-picture">
+                    <img src="image/userpage/pp.png" alt="Profile Picture">
+                </div>
+            </nav>
+        <div class="user-post-wrapper">
+            <section class="user-profile">
+                <div class="pp">
+                    <img src="image/userpage/pp2.png" alt="User Profile Picture">
+                </div>
+                <div class="user-info">
+                    <h2>Mr. Chadest</h2>
+                    <p>10 post</p>
+                    <form action="editprofile##" class="style-button">
+                        <button type="submit"><i class="fa-solid fa-pen"></i>Edit profile</button>
+                    </form>
+                </div>
+            </section>
+            <aside class="my-post">
+                <div class="my-postingan">
+                    <h3>My Posts</h3>
+                    <hr>
+                </div>
+                <div class="post-wrapper">
+                    <div class="wrapper1">
+                        <div class="informasi">
+                            <img src="image/userpage/pp2.png" alt="Profile foto"> 
+                            <p>Mr. Chadest</p>
+                        </div>
+                        <div class="text1" id="post-1">
+                            <hr>
+                            <p>Dude I own this NFT. Do you really think you can get away with theft when you’re showing what you stole directly to my face. My lawyers will make an easy job of this case. Prepare to say goodbye to your luscious life and start preparing for the streets. I will ruin you.</p>
+                        </div>
+                    </div>
+                    <div class="wrapper2">
+                        <div class="informasi">
+                            <img src="image/userpage/pp2.png" alt="Profile foto"> 
+                            <p>Mr. Chadest</p>
+                        </div>
+                            <img src="image/userpage/postingan.png" alt="post-1">
+                    </div>
+                </div>
+            </aside>
+        </div>
     </body>
     <script src="https://kit.fontawesome.com/ca8cef09e9.js" crossorigin="anonymous"></script>
 </html>
